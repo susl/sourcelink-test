d189618b455b981e6a37bc0667f635a5b27e71a0
Dmytro Suvorov <suslamo@gmail.com>
2015-06-12 16:34:38 +0300

    add sourcelink

--- build.fsx
+++ build.fsx
@@ -1,11 +1,57 @@
-// include Fake lib
-#r @"packages/FAKE/tools/FakeLib.dll"
-open Fake
+#r "packages/FAKE/tools/FakeLib.dll"
+#load "packages/SourceLink.Fake/tools/SourceLink.fsx"
 
-// Default target
-Target "Default" (fun _ ->
-    trace "Hello World from FAKE"
+open System
+open System.IO
+open Fake
+open Fake.AssemblyInfoFile
+open SourceLink
+ 
+let versionAssembly = "1.0.2.0"
+let versionFile = "1.0.2.1"
+ 
+Target "Clean" (fun _ -> !! "**/bin/" ++ "**/obj/" |> CleanDirs)
+ 
+Target "AssemblyInfo" (fun _ ->
+    CreateCSharpAssemblyInfo "AssemblyInfoCommon.cs"
+        [   Attribute.Version versionAssembly
+            Attribute.FileVersion versionFile ]
+)
+ 
+Target "Build" (fun _ ->
+    !! "Nuget.Debug.Test/Nuget.Debug.Test.csproj"
+    |> MSBuildRelease "" "Rebuild" |> ignore
 )
 
-// start build
-RunTargetOrDefault "Default"
+Target "SourceLink" (fun _ ->
+    use repo = new GitRepo(__SOURCE_DIRECTORY__)
+    [ "Nuget.Debug.Test/Nuget.Debug.Test.csproj" ]
+    |> Seq.iter (fun pf ->
+        let proj = VsProj.LoadRelease pf
+        logfn "source linking %s" proj.OutputFilePdb
+        let files = (proj.Compiles -- "SolutionInfo.cs").SetBaseDirectory __SOURCE_DIRECTORY__
+        repo.VerifyChecksums files
+        proj.VerifyPdbChecksums files
+        proj.CreateSrcSrv "https://raw.githubusercontent.com/susl/sourcelink-test/{0}/%var2%" repo.Commit (repo.Paths files)
+        Pdbstr.exec proj.OutputFilePdb proj.OutputFilePdbSrcSrv
+    )
+)
+ 
+Target "NuGet" (fun _ ->
+    let bin = "../bin"
+    Directory.CreateDirectory bin |> ignore
+    NuGet (fun p -> 
+        { p with
+            Version = versionFile
+            WorkingDir = "Nuget.Debug.Test/bin/Release"
+            OutputPath = bin
+        }) "Nuget.Debug.Test/Nuget.Debug.Test.nuspec"
+)
+ 
+"Clean"
+//    ==> "AssemblyInfo"
+    ==> "Build"
+    ==> "SourceLink"
+    ==> "NuGet"
+ 
+RunTargetOrDefault "NuGet"
