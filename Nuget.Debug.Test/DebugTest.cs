18b82a5a2e399844c3a83e353e939ac55ea8526d
Dmytro Suvorov <suslamo@gmail.com>
2015-06-12 16:01:15 +0300

    initial

---
+++ Nuget.Debug.Test/DebugTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Nuget.Debug.Test
+{
+    using System.Globalization;
+
+    public class DebugTest
+    {
+        public static string SomeMethod(int i)
+        {
+            var s = i.ToString(CultureInfo.InvariantCulture);
+            s += "$";
+            return s;
+        }
+    }
+}
---
+++ Nuget.Debug.Test/push.cmd
@@ -0,0 +1,2 @@
+nuget push Nuget.Debug.Test.1.0.0.0.nupkg PushPackagesApiKey -s http://bkiev-payqa-app.betsson.local:4333/nuget/Betsson.Payments
+nuget push Nuget.Debug.Test.1.0.0.0.symbols.nupkg  PushPackagesApiKey -s http://bkiev-payqa-app.betsson.local:4333/nuget/Betsson.Payments
